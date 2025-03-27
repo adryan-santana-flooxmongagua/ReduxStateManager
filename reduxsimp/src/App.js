21768a448a41f3be9b7bd2c498b514b70954e448
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-27 13:50:48 -0300

    feat: create Inteval component

--- reduxsimp/src/App.js
+++ reduxsimp/src/App.js
@@ -1,31 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
 
 function App() {
   return (
-<div className="App">
+    <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card title="Card 1 " red>
-          x
-        </Card>
-   
-      <Card title="Card 2" purple>
-          y
-        </Card>
-        
+        <Intervalo> </Intervalo>
       </div>
-      <div className="linha" >
-      <Card title="Card 3" green>
+      <div className="linha">
+        <Card title="Card 3" green>
           y
         </Card>
-        
-    
-      <Card title="Card 4" blue>
+
+        <Card title="Card 4" blue>
           y
         </Card>
-        
       </div>
     </div>
   );
