1965f1bff15be2a54078ba61a5dc9d7c04b4bb3b
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-27 14:07:12 -0300

    feat: add new components

--- reduxsimp/src/App.js
+++ reduxsimp/src/App.js
@@ -1,23 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./components/Card";
-import Intervalo from "./components/Intervalo";
+import React from 'react';
+import './App.css';
+
+import Intervalo from './components/Intervalo'
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio from './components/Sorteio';
 
 function App() {
+
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Intervalo> </Intervalo>
+        <Intervalo></Intervalo>
       </div>
       <div className="linha">
-        <Card title="Card 3" green>
-          y
-        </Card>
-
-        <Card title="Card 4" blue>
-          y
-        </Card>
+        <Media></Media>
+        <Soma></Soma>
+        <Sorteio></Sorteio>
       </div>
     </div>
   );
